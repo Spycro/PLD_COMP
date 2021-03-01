3be9710b254592684512a5ff5ec16594ac056526
Charles JAVERLIAT <charles.javerliat@gmail.com>
2021-03-01 17:26:39 +0100

    Update visitor

--- compiler/visitor.cpp
+++ compiler/visitor.cpp
@@ -1,7 +1,86 @@
 
 // Generated from ifcc.g4 by ANTLR 4.7.2
 
-
 #include "visitor.h"
 
+antlrcpp::Any Visitor::visitProg(ifccParser::ProgContext *ctx) {
 
+  // Prologue of assembly code
+  std::cout << ".global	main\n"
+            << " main: \n"
+            << "  pushq %rbp\n"
+            << "  movq %rsp, %rbp\n";
+
+  // Visit all declarations
+  for (ifccParser::DeclarationContext *declarationCtx : ctx->declaration()) {
+    this->visitDeclaration(declarationCtx);
+  }
+
+  // Final return of the main function
+  this->visitRetValue(ctx->retValue());
+  std::cout << "  popq %rbp\n"
+            << "  ret\n";
+
+  return 0;
+}
+
+antlrcpp::Any Visitor::visitRetValue(ifccParser::RetValueContext *ctx) {
+
+  std::string rval;
+
+  if (ctx->CONST()) {
+    std::cout << "  movl $" << ctx->CONST()->getText() << ", %eax\n";
+  } else if (ctx->VAR_NAME()) {
+    int i = getVarStackIndex(ctx->VAR_NAME()->getText());
+    std::cout << "  movl -" << (i + 1) * 4 << "(%rbp), %eax\n";
+  }
+
+  return 0;
+}
+
+antlrcpp::Any Visitor::visitDeclaration(ifccParser::DeclarationContext *ctx) {
+
+  std::string variableName = ctx->VAR_NAME()->getText();
+
+  /*
+   * Add the variable name to the list of variables.
+   * Used to keep track of var index on the stack.
+   *
+   * TODO: keep track of variable type, visibility, etc..
+   */
+  this->varAddresses.emplace(variableName, stackSize);
+  this->visitValue(ctx->value());
+  ++stackSize;
+
+  return 0;
+}
+
+antlrcpp::Any Visitor::visitValue(ifccParser::ValueContext *ctx) {
+
+  if (ctx->CONST()) {
+
+    int val = stoi(ctx->CONST()->getText());
+    std::cout << "  movl $" << val << ", -" << (stackSize + 1) * 4 << "(%rbp)\n";
+
+  } else if (ctx->VAR_NAME()) {
+
+    int addr = getVarStackIndex(ctx->VAR_NAME()->getText());
+    std::cout << "  movl -" << (addr + 1) * 4 << "(%rbp), %eax\n";
+    std::cout << "  movl %eax, "
+              << "-" << (stackSize + 1) * 4 << "(%rbp)\n";
+  }
+
+  return 0;
+}
+
+int Visitor::getVarStackIndex(std::string variableName) {
+
+  auto it = varAddresses.find(variableName);
+
+  if (it == varAddresses.end()) {
+    std::cerr << "Variable not found '" << variableName << "'.";
+    return -1;
+  } else {
+    return it->second;
+  }
+}
--- compiler/visitor.h
+++ compiler/visitor.h
@@ -3,33 +3,38 @@
 
 #pragma once
 
-#include "antlr4-runtime.h"
 #include "antlr4-generated/ifccVisitor.h"
+#include "antlr4-runtime.h"
 
 /**
  * This class provides an empty implementation of ifccVisitor, which can be
  * extended to create a visitor which only needs to handle a subset of the available methods.
  */
-class Visitor : public ifccVisitor
-{
+class Visitor : public ifccVisitor {
 public:
-  virtual antlrcpp::Any visitAxiom(ifccParser::AxiomContext *ctx) override
-  {
-    return visitChildren(ctx);
-  }
+  virtual antlrcpp::Any visitAxiom(ifccParser::AxiomContext *ctx) override { return visitChildren(ctx); }
 
-  virtual antlrcpp::Any visitProg(ifccParser::ProgContext *ctx) override
-  {
+  virtual antlrcpp::Any visitProg(ifccParser::ProgContext *ctx) override;
 
-    int retval = stoi(ctx->CONST()->getText());
-    std::cout << ".global	main\n"
-              << " main: \n"
-              << "  pushq %rbp\n"
-              << "  movq %rsp, %rbp\n"
-              << "  movl	$" << retval << ", %eax\n"
-              << "  popq %rbp\n"
-              << "  ret\n";
+  virtual antlrcpp::Any visitDeclaration(ifccParser::DeclarationContext *ctx) override;
 
-    return 0;
-  }
+  virtual antlrcpp::Any visitValue(ifccParser::ValueContext *ctx) override;
+
+  virtual antlrcpp::Any visitRetValue(ifccParser::RetValueContext *ctx) override;
+
+  /**
+   * Return the variable address, given its name.
+   */
+  int getVarStackIndex(std::string varName);
+
+private:
+  /**
+   * Store the address of each declared variable.
+   *
+   * The key is the variable name, the value is the address.
+   * The adress is relative to the bottom of the stack (starts at 0).
+   */
+  std::unordered_map<std::string, int> varAddresses;
+
+  int stackSize = 0;
 };
