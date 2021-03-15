22b000eca98006a1a8f3c1c3d5b6edafcc0fee74
Leong Loris <lleong@if501-219-11.insa-lyon.fr>
2021-03-15 08:20:43 +0100

    compile_if

--- compile_if.sh
+++ compile_if.sh
@@ -1,1 +1,1 @@
-make ANTLR4_BINDIR=/usr/bin LIBDIR=/shares/public/tp/ANTLR4-CPP/lib INCDIR=/shares/public/tp/ANTLR4-CPP/antlr4-runtime
+make ANTLR4_BINDIR=/usr/bin ANTLR4_LIBDIR=/shares/public/tp/ANTLR4-CPP/lib ANTLR4_INCDIR=/shares/public/tp/ANTLR4-CPP/antlr4-runtime
