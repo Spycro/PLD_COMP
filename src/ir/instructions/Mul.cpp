39deb9ec36dadb37c2f4732c9a786235a6482842
Charles Javerliat <charles.javerliat@gmail.com>
2021-04-07 11:41:22 +0200

    internal: add multiply

--- src/ir/instructions/Mul.cpp
+++ src/ir/instructions/Mul.cpp
@@ -1,19 +1,20 @@
 #include "ir/instructions/Mul.h"
 #include "ir/ASMConstants.h"
+#include "ir/ASMx86Utils.h"
+
+using namespace ASMx86Utils;
 
 Mul::Mul(BasicBlock *bb, SymbolTableElement a_, SymbolTableElement b_, SymbolTableElement res_) : IRInstr(bb), a(a_), b(b_), res(res_){}
 
 
 void Mul::gen_asm(std::ostream &o) {
+    // Get subregister of RAX (al, ax, eax) depending on res size
+    SymbolTableElement& raxSubReg = getSubReg(RAX_REGISTER, res.getSize());
 
-    std::string val1, val2, dest;
-
-    val1 = a.getAsm();
-    val2 = b.getAsm();
-    dest = res.getAsm();
-
-    o << "\tmovq " << val1 << ", " << RAX_REGISTER.getAsm() <<std::endl;
-    o << "\timulq " << val2 << ", " << RAX_REGISTER.getAsm() << std::endl;
-    o << "\tmovq " << RAX_REGISTER.getAsm() << ", " << dest << std::endl;
-
+    // Convert both operands to 64 bits
+    o << moveTo64Reg(a, RAX_REGISTER) << std::endl;
+    o << moveTo64Reg(b, RDX_REGISTER) << std::endl;
+    // Perform 64 bits multiplication
+    o << "\timulq " << RDX_REGISTER.getAsm() << ", " << RAX_REGISTER.getAsm() << std::endl;
+    o << "\tmov" << symbolSizeChar(res.getSize()) << " " << raxSubReg.getAsm() << ", " << res.getAsm() << std::endl;
 }
