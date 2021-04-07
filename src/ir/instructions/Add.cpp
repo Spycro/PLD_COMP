2084df53f4f042ce9ef589db70f40bfe63d2cde3
Charles Javerliat <charles.javerliat@gmail.com>
2021-04-07 11:34:51 +0200

    feat: add operation

--- src/ir/instructions/Add.cpp
+++ src/ir/instructions/Add.cpp
@@ -1,19 +1,21 @@
 #include "ir/instructions/Add.h"
+#include "ir/ASMx86Utils.h"
 #include "ir/ASMConstants.h"
+
+using namespace ASMx86Utils;
 
 Add::Add(BasicBlock *bb, SymbolTableElement a_, SymbolTableElement b_, SymbolTableElement res_) : IRInstr(bb), a(a_), b(b_), res(res_){}
 
 
 void Add::gen_asm(std::ostream &o) {
+    // Get subregister of RAX (al, ax, eax) depending on res size
+    SymbolTableElement& raxSubReg = getSubReg(RAX_REGISTER, res.getSize());
 
-    std::string val1, val2, dest;
+    // Convert both operands to 64 bits
+    o << moveTo64Reg(a, RAX_REGISTER) << std::endl;
+    o << moveTo64Reg(b, RDX_REGISTER) << std::endl;
 
-    val1 = a.getAsm();
-    val2 = b.getAsm();
-    dest = res.getAsm();
-
-    o << "\tmovq " << val2 << ", " << RAX_REGISTER.getAsm() <<std::endl;
-    o << "\taddq " << val1 << ", " << RAX_REGISTER.getAsm() << std::endl;
-    o << "\tmovq " << RAX_REGISTER.getAsm() << ", " << dest << std::endl;
-
+    // Perform 64 bits addition
+    o << "\taddq " << RDX_REGISTER.getAsm() << ", " << RAX_REGISTER.getAsm() << std::endl;
+    o << "\tmov" << symbolSizeChar(res.getSize()) << " " << raxSubReg.getAsm() << ", " << res.getAsm() << std::endl;
 }
