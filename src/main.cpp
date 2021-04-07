e58cf63763ce1a1aef4080171ec1993a98057cb1
Charles Javerliat <charles.javerliat@gmail.com>
2021-04-07 21:27:54 +0200

    feat: add error handling for lexer

--- src/main.cpp
+++ src/main.cpp
@@ -18,17 +18,51 @@ void PrintScope(shared_ptr<Scope>, int count);
 
 class MyErrorListener : public BaseErrorListener {
 public:
-  MyErrorListener() { error = false; }
-  bool Error() { return error; }
-  virtual void syntaxError(Recognizer *recognizer, Token *offendingSymbol,
-                           size_t line, size_t charPositionInLine,
-                           const std::string &msg, std::exception_ptr e) {
-    cout << "Error on position " << line << ":" << charPositionInLine << endl;
-    error = true;
-  }
+    MyErrorListener() { error = false; }
+    bool hasError() { return error; }
+    virtual void syntaxError(Recognizer *recognizer, Token *offendingSymbol,
+                            size_t line, size_t charPositionInLine,
+                            const std::string &msg, std::exception_ptr e) {
+        cerr << "Error on position " << line << ":" << charPositionInLine << endl;
+        error = true;
+    }
 
 protected:
-  bool error;
+    bool error;
+};
+
+class LexerErrorListener : public ANTLRErrorListener {
+public:
+
+    LexerErrorListener() { error = false; }
+    bool hasError() { return error; }
+
+    virtual void syntaxError(Recognizer *recognizer, Token *offendingSymbol, size_t line,
+                                size_t charPositionInLine, const std::string &msg, std::exception_ptr e) {
+        cerr << "Syntax error " << line << ":" << charPositionInLine << endl;
+        error = true;
+    }
+
+    virtual void reportAmbiguity(Parser *recognizer, const dfa::DFA &dfa, size_t startIndex, size_t stopIndex, bool exact,
+                                const antlrcpp::BitSet &ambigAlts, atn::ATNConfigSet *configs) {
+        cerr << "Ambiguity at " << startIndex << ":" << stopIndex << endl;
+        error = true;
+    }
+
+    virtual void reportAttemptingFullContext(Parser *recognizer, const dfa::DFA &dfa, size_t startIndex, size_t stopIndex,
+                                            const antlrcpp::BitSet &conflictingAlts, atn::ATNConfigSet *configs) {
+        cerr << "Attempting full context " << startIndex << ":" << stopIndex << endl;
+        error = true;
+    }
+
+    virtual void reportContextSensitivity(Parser *recognizer, const dfa::DFA &dfa, size_t startIndex, size_t stopIndex,
+                                            size_t prediction, atn::ATNConfigSet *configs) {
+        cerr << "Context sensitivity " << startIndex << ":" << stopIndex << endl;
+        error = true;
+    }
+
+protected:
+    bool error;
 };
 
 int main(int argn, const char **argv) {
@@ -39,12 +73,17 @@ int main(int argn, const char **argv) {
   }
   ANTLRInputStream input(in.str());
   ifccLexer lexer(&input);
+
+  LexerErrorListener lexerErrorListener;
+  lexer.removeErrorListeners();
+  lexer.addErrorListener(&lexerErrorListener);
+
   CommonTokenStream tokens(&lexer);
 
   tokens.fill();
-  //  for (auto token : tokens.getTokens()) {
-  //    std::cout << token->toString() << std::endl;
-  //  }
+
+if (lexerErrorListener.hasError())
+    return 1;
 
   ifccParser parser(&tokens);
 
@@ -53,7 +92,7 @@ int main(int argn, const char **argv) {
   parser.addErrorListener(&errorlistener);
 
   tree::ParseTree *tree = parser.axiom();
-  if (errorlistener.Error())
+  if (errorlistener.hasError())
     return 1;
 
   Visitor visitor;
