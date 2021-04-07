ef21fee44a736ca9a8ddcc7d31d808583720c343
Charles Javerliat <charles.javerliat@gmail.com>
2021-04-07 21:34:33 +0200

    chores: refactor a bit error listener

--- src/main.cpp
+++ src/main.cpp
@@ -16,48 +16,14 @@ using namespace std;
 void PrintAst(shared_ptr<Node> node, int count);
 void PrintScope(shared_ptr<Scope>, int count);
 
-class MyErrorListener : public BaseErrorListener {
+class ErrorListener : public BaseErrorListener {
 public:
-    MyErrorListener() { error = false; }
+    ErrorListener() { error = false; }
     bool hasError() { return error; }
     virtual void syntaxError(Recognizer *recognizer, Token *offendingSymbol,
                             size_t line, size_t charPositionInLine,
                             const std::string &msg, std::exception_ptr e) {
-        cerr << "Error on position " << line << ":" << charPositionInLine << endl;
-        error = true;
-    }
-
-protected:
-    bool error;
-};
-
-class LexerErrorListener : public ANTLRErrorListener {
-public:
-
-    LexerErrorListener() { error = false; }
-    bool hasError() { return error; }
-
-    virtual void syntaxError(Recognizer *recognizer, Token *offendingSymbol, size_t line,
-                                size_t charPositionInLine, const std::string &msg, std::exception_ptr e) {
-        cerr << "Syntax error " << line << ":" << charPositionInLine << endl;
-        error = true;
-    }
-
-    virtual void reportAmbiguity(Parser *recognizer, const dfa::DFA &dfa, size_t startIndex, size_t stopIndex, bool exact,
-                                const antlrcpp::BitSet &ambigAlts, atn::ATNConfigSet *configs) {
-        cerr << "Ambiguity at " << startIndex << ":" << stopIndex << endl;
-        error = true;
-    }
-
-    virtual void reportAttemptingFullContext(Parser *recognizer, const dfa::DFA &dfa, size_t startIndex, size_t stopIndex,
-                                            const antlrcpp::BitSet &conflictingAlts, atn::ATNConfigSet *configs) {
-        cerr << "Attempting full context " << startIndex << ":" << stopIndex << endl;
-        error = true;
-    }
-
-    virtual void reportContextSensitivity(Parser *recognizer, const dfa::DFA &dfa, size_t startIndex, size_t stopIndex,
-                                            size_t prediction, atn::ATNConfigSet *configs) {
-        cerr << "Context sensitivity " << startIndex << ":" << stopIndex << endl;
+        cerr << "Error at " << line << ":" << charPositionInLine << endl;
         error = true;
     }
 
@@ -66,54 +32,58 @@ protected:
 };
 
 int main(int argn, const char **argv) {
-  stringstream in;
-  if (argn == 2) {
-    ifstream lecture(argv[1]);
-    in << lecture.rdbuf();
-  }
-  ANTLRInputStream input(in.str());
-  ifccLexer lexer(&input);
 
-  LexerErrorListener lexerErrorListener;
-  lexer.removeErrorListeners();
-  lexer.addErrorListener(&lexerErrorListener);
+    stringstream in;
 
-  CommonTokenStream tokens(&lexer);
+    if (argn == 2) {
+        ifstream lecture(argv[1]);
+        in << lecture.rdbuf();
+    }
 
-  tokens.fill();
+    ANTLRInputStream input(in.str());
+    ifccLexer lexer(&input);
 
-if (lexerErrorListener.hasError())
-    return 1;
+    ErrorListener errorListener;
 
-  ifccParser parser(&tokens);
+    lexer.removeErrorListeners();
+    lexer.addErrorListener(&errorListener);
 
-  MyErrorListener errorlistener;
-  parser.removeErrorListeners();
-  parser.addErrorListener(&errorlistener);
+    CommonTokenStream tokens(&lexer);
 
-  tree::ParseTree *tree = parser.axiom();
-  if (errorlistener.hasError())
-    return 1;
+    tokens.fill();
 
-  Visitor visitor;
-  visitor.visit(tree);
+    if (errorListener.hasError())
+        return 1;
 
-  shared_ptr<Node> rootNode = visitor.getRootNode();
+    ifccParser parser(&tokens);
 
-  #ifdef DEBUG
-  PrintAst(rootNode, 0);
-  #endif
-  
-  if(visitor.getErrorFlag()){
-    std::cerr << visitor.getErrorTrace() << std::endl;
-    return 1;
-  }
+    parser.removeErrorListeners();
+    parser.addErrorListener(&errorListener);
 
-  //IR
-  IR myIR(rootNode);
-  
+    tree::ParseTree *tree = parser.axiom();
+    
+    if (errorListener.hasError())
+        return 1;
 
-  return 0;
+    Visitor visitor;
+    visitor.visit(tree);
+
+    shared_ptr<Node> rootNode = visitor.getRootNode();
+
+    #ifdef DEBUG
+    PrintAst(rootNode, 0);
+    #endif
+    
+    if(visitor.getErrorFlag()){
+        std::cerr << visitor.getErrorTrace() << std::endl;
+        return 1;
+    }
+
+    //IR
+    IR myIR(rootNode);
+    
+
+    return 0;
 }
 
 
