e0003bd956ede487997a63c840e01d03baea82f9
PeutySam <ubuntu@ubuntu1804.linuxvmimages.com>
2021-04-07 09:53:36 -0400

    feat: fibonaci file

---
+++ tests/test_cases/v2/58_Fibonaci.c
@@ -0,0 +1,21 @@
+int main() {
+    int n = 10;
+    int i = 0;
+    int c;
+    int result;
+
+    for(c=1;c<=n;c++) {
+        result = f(i);
+        i++;
+    }
+
+    return result;
+}
+
+int f (int n) {
+    if(n == 0 || n == 1) {
+        return n;
+    } else {
+        return(f(n-1) + f(n-2));
+    }
+}
