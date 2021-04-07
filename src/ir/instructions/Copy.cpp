4815322f00955843e541ea4138bf487bb2816124
Charles Javerliat <charles.javerliat@gmail.com>
2021-04-07 15:54:59 +0200

    docs: add copy todo

--- src/ir/instructions/Copy.cpp
+++ src/ir/instructions/Copy.cpp
@@ -6,11 +6,12 @@ using namespace ASMx86Utils;
 
 Copy::Copy(BasicBlock* bb, SymbolTableElement inputX, SymbolTableElement inputD) : IRInstr(bb), d(inputD), x(inputX){}
 
+//TODO: optimize to avoid using 2 instr instead of 1 in the case where a const is put into a stack var
 void Copy::gen_asm(std::ostream &o) {
     // Get subregister of RAX (al, ax, eax) depending on dest size
     SymbolTableElement& raxSubReg = getSubReg(RAX_REGISTER, d.getSize());
 
     // Move (and convert) x to 64 bits RAX register
     o << moveTo64Reg(x, RAX_REGISTER) << std::endl;
-    o << "\tmov" << symbolSizeChar(d.getSize()) << " " << raxSubReg.getAsm() << ", " << d.getAsm() << std::endl;   
+    o << "\tmov" << symbolSizeChar(d.getSize()) << " " << raxSubReg.getAsm() << ", " << d.getAsm() << std::endl;
 }
